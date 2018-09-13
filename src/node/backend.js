2fc711a3bbe6128e38a51f3fbda8a6493f10c1db
Minser <Alexander.Minser@Honeywell.com>
2018-09-13 10:54:25 -0400

    File and css stuff added

--- src/node/backend.js
+++ src/node/backend.js
@@ -1,4 +1,14 @@
 // backend node js stuff
 // Alexander Minser
 
+var http = require('http')
+var fs = require('fs')
 
+
+fs.readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
