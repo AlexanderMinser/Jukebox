bee602a59b83685d52212fc6b057a4b07ceac874
Alex <adminser@ncsu.edu>
2018-09-15 07:48:26 -0400

    changes from yesterday

--- src/index.js
+++ src/index.js
@@ -4,16 +4,27 @@ const http = require('http')
 const fs = require('fs')
 const express = require('express')
 const path = require('path');
-
+const bodyParser = require('body-parser')
 const app = express()
 
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+ }));
+ app.use(bodyParser.json());
+
 
 app.get('/', function(req, res){
     res.sendFile('public/index.html');
-    var myText = req.query.mytext;
-    console.log('there was a request for ${}myText')
  });
+
+ app.post('/',function(req,res){
+    if (req.body.hasOwnProperty('button1')) {
+        const myText = req.query.mytext;
+        res.send('there was a request for '+myText);
+    }
+    console.log(req.headers.origin)
+ })
  
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
